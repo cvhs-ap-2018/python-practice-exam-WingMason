c8c03dabb09181349209f4032f364bcf948d6a1f
WingMason <30908002+WingMason@users.noreply.github.com>
2017-11-09 11:54:46 -0800

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -2,12 +2,16 @@
 1. Write the lines of code that would import
 and create a turtle named 'Pong'.
 """
+import turtle
+pong = turtle.Turtle()
 
 
 """
 2.  Draw a square with Pong of length 100
 """
-
+for i in range(4):
+  pong.fd(100)
+  pong.right(90)
 
 
 """
@@ -15,12 +19,19 @@ and create a turtle named 'Pong'.
 to input the length of a square and then draw
 a square of that length.
 """
-
+def sqr():
+  x = int(input())
+  for i in range(4):
+    pong.fd(x)
+    pong.rihgt(90)
 
 """
 4.  Draw a series of squares with different sizes
 that share the same corner.
 """
+
+
+
 
 
 """
